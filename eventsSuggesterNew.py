41793ad264db0029ff8ee6d774a776f9ad1dc195
lkkcharlie0126 <lkkcharlie0126@gmail.com>
2023-02-04 08:57:01 +0000

    refactor: candidate generation code & notebook

--- eventsSuggesterNew.py
+++ eventsSuggesterNew.py
@@ -1,8 +1,6 @@
 import itertools
 from collections import Counter
 import numpy as np
-from operator import itemgetter
-import gensim
 import math
 import heapq
 
@@ -46,22 +44,6 @@ class EventsSuggester:
     def reRank20up(self):
         # RERANK CANDIDATES
         result = [aid for aid, cnt in self.candidates.most_common(self.topN)]
-        # if (len(result) < self.topN):
-        #     resultSet = set(result)
-        #     w2vScore = []
-        #     for aid in self.topEvents:
-        #         if aid not in resultSet:
-        #             if len(w2vScore) < (self.topN - len(result)):
-        #                 heapq.heappush(w2vScore, (-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #             else:
-        #                 heapq.heappushpop(w2vScore, (-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #                 # w2vScore.append((-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #     w2vScore.sort(reverse=True)
-        #     idx = 0
-        #     while(len(result) < self.topN):
-        #         result.append(w2vScore[idx][1])
-        #         idx += 1
-
         resultSet = set(result)
         idx = 0
         while(len(result) < self.topN):
@@ -80,53 +62,7 @@ class EventsSuggester:
             if self.topEvents[idx] not in resultSet:
                 result.append(self.topEvents[idx])
             idx += 1
-        # if (len(result) < self.topN):
-        #     resultSet = set(result)
-        #     w2vScore = []
-        #     for aid in self.topEvents:
-        #         if aid not in resultSet:
-        #             if len(w2vScore) < (self.topN - len(result)):
-        #                 heapq.heappush(w2vScore, (-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #             else:
-        #                 heapq.heappushpop(w2vScore, (-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #     w2vScore.sort(reverse=True)
-        #     idx = 0
-        #     while(len(result) < self.topN):
-        #         result.append(w2vScore[idx][1])
-        #         idx += 1
-
         return result[:self.topN]
-
-
-
-# class ClicksSuggester(EventsSuggester):
-
-#     def __init__(self, topEvents, top_click2click, top_n_clicks):
-#         self.topEvents = list(topEvents)
-#         self.top_click2click = top_click2click
-#         self.top_n_clicks = top_n_clicks
-
-#     def dfProcess(self):
-#         super().dfProcess()
-#         self.df = self.df.loc[self.df['type']==0]
-#         self.unique_clicks = list(dict.fromkeys( self.df.aid.tolist()[::-1] ))
-
-#     def candidatesGenerate20up(self):
-#         super().candidatesGenerate20up()
-#         # RERANK CANDIDATES USING "BUY2BUY" CO-VISITATION MATRIX
-#         aids2 = list(itertools.chain(*[self.top_n_clicks[aid] for aid in self.unique_aids if aid in self.top_n_clicks][:2]))
-#         aids3 = list(itertools.chain(*[self.top_click2click[aid] for aid in self.unique_aids if aid in self.top_click2click][:5]))
-#         for aid in aids3 + aids2: 
-#             self.candidates[aid] += 0.1
-
-#     def candidatesGenerate(self):
-#         # USE "CLICKS" CO-VISITATION MATRIX
-#         aids2 = list(itertools.chain(*[self.top_n_clicks[aid] for aid in self.unique_aids if aid in self.top_n_clicks][:2]))
-#         aids3 = list(itertools.chain(*[self.top_click2click[aid] for aid in self.unique_clicks if aid in self.top_click2click][:4]))
-#         self.candidates = Counter(aids2+aids3)
-#         for aid in self.unique_aids:
-#             if aid in self.candidates:
-#                 del self.candidates[aid]
 
 class ClicksSuggester(EventsSuggester):
 
@@ -165,44 +101,6 @@ class ClicksSuggester(EventsSuggester):
 
         for aid in self.candidates:
             self.candidates[aid] *= -math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem))
-
-# class ClicksSuggester(EventsSuggester):
-
-#     def __init__(self, topEvents, top_click2click, top_n_clicks, aid2vec_model):
-#         self.topEvents = list(topEvents)
-#         self.top_click2click = top_click2click
-#         self.top_n_clicks = top_n_clicks
-#         self.aid2vec_model = aid2vec_model
-
-
-#     def dfProcess(self):
-#         super().dfProcess()
-#         self.df = self.df.loc[self.df['type']==0]
-#         self.unique_clicks = list(dict.fromkeys( self.df.aid.tolist()[::-1] ))
-
-#     def candidatesGenerate20up(self):
-#         super().candidatesGenerate20up()
-#         # RERANK CANDIDATES USING "BUY2BUY" CO-VISITATION MATRIX
-#         aids2 = list(itertools.chain(*[self.top_n_clicks[aid] for aid in self.unique_aids if aid in self.top_n_clicks][:2]))
-#         aids3 = list(itertools.chain(*[self.top_click2click[aid] for aid in self.unique_aids if aid in self.top_click2click][:5]))
-#         for aid in aids3 + aids2: 
-#             self.candidates[aid] += 0.1
-#         for aid in self.candidates:
-#             self.candidates[aid] *= -math.log(max(1 - self.aid2vec_model.similarity(str(aid), self.lastItem), 0.000001))
-
-#     def candidatesGenerate(self):
-#         # USE "CLICKS" CO-VISITATION MATRIX
-#         aids2 = list(itertools.chain(*[self.top_n_clicks[aid] for aid in self.unique_aids if aid in self.top_n_clicks][:2]))
-#         aids3 = list(itertools.chain(*[self.top_click2click[aid] for aid in self.unique_clicks if aid in self.top_click2click][:4]))
-
-#         self.candidates = Counter(aids2+aids3)
-
-#         for aid in self.unique_aids:
-#             if aid in self.candidates:
-#                 del self.candidates[aid]
-
-#         for aid in self.candidates:
-#             self.candidates[aid] *= -math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem))
 
 
 
@@ -286,24 +184,6 @@ class BuysSuggester(EventsSuggester):
     def reRank20up(self):
         # RERANK CANDIDATES
         result = self.unique_carts + [aid for aid, _ in self.candidates.most_common(self.topN - len(self.unique_carts))]
-
-        # result = [aid for aid, _ in self.candidates.most_common(self.topN)]
-        # if (len(result) < self.topN):
-        #     resultSet = set(result)
-        #     w2vScore = []
-        #     for aid in self.topEvents:
-        #         if aid not in resultSet:
-        #             if len(w2vScore) < (self.topN - len(result)):
-        #                 heapq.heappush(w2vScore, (-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #             else:
-        #                 heapq.heappushpop(w2vScore, (-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #                 # w2vScore.append((-math.log(1 - self.aid2vec_model.similarity(str(aid), self.lastItem)), aid))
-        #     w2vScore.sort(reverse=True)
-        #     idx = 0
-        #     while(len(result) < self.topN):
-        #         result.append(w2vScore[idx][1])
-        #         idx += 1
-
         resultSet = set(result)
         idx = 0
         while(len(result) < self.topN):
@@ -331,12 +211,6 @@ class BuysSuggester(EventsSuggester):
         # RERANK CANDIDATES
         result = self.unique_aids + [aid for aid, _ in self.candidates.most_common(self.topN - len(self.unique_aids))]
         # USE TOP20 TEST CLICKS
-        # idx = 0
-        # resultSet = set(result)
-        # while(len(result) < self.topN):
-        #     if self.topEvents[idx] not in resultSet:
-        #         result.append(self.topEvents[idx])
-        #     idx += 1
         if (len(result) < self.topN):
             resultSet = set(result)
             w2vScore = []
